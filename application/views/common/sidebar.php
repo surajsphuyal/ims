5998009dfd0fe9ccf2ef4d8f37a131a56a192ef5
JanakRaj Chapagain <janakraj.chapagain@apexcollege.edu.np>
2020-02-07 10:28:39 +0545

    header footer and siderbar template

---
+++ application/views/common/sidebar.php
@@ -0,0 +1,83 @@
+
+<!-- BEGIN CONTAINER -->
+<div class="page-container">
+	<!-- BEGIN SIDEBAR -->
+	<div class="page-sidebar-wrapper">
+		<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+		<!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+		<div class="page-sidebar navbar-collapse collapse">
+			<!-- BEGIN SIDEBAR MENU -->
+			<!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
+			<!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
+			<!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+			<!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+			<!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
+			<!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+			<ul class="page-sidebar-menu page-sidebar-menu-hover-submenu " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+				<li class="start ">
+					<a href="index.html">
+					<i class="icon-home"></i>
+					<span class="title">Dashboard</span>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:;">
+					<i class="icon-folder"></i>
+					<span class="title">Multi Level Menu</span>
+					<span class="arrow "></span>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="javascript:;">
+							<i class="icon-settings"></i> Item 1 <span class="arrow"></span>
+							</a>
+							<ul class="sub-menu">
+								<li>
+									<a href="javascript:;">
+									<i class="icon-user"></i>
+									Sample Link 1 <span class="arrow"></span>
+									</a>
+									<ul class="sub-menu">
+										<li>
+											<a href="#"><i class="icon-power"></i> Sample Link 1</a>
+										</li>
+										<li>
+											<a href="#"><i class="icon-paper-plane"></i> Sample Link 1</a>
+										</li>
+										<li>
+											<a href="#"><i class="icon-star"></i> Sample Link 1</a>
+										</li>
+									</ul>
+							</ul>
+						</li>
+						<li>
+							<a href="javascript:;">
+							<i class="icon-globe"></i> Item 2 <span class="arrow"></span>
+							</a>
+							<ul class="sub-menu">
+								<li>
+									<a href="#"><i class="icon-tag"></i> Sample Link 1</a>
+								</li>
+								<li>
+									<a href="#"><i class="icon-pencil"></i> Sample Link 1</a>
+								</li>
+								<li>
+									<a href="#"><i class="icon-graph"></i> Sample Link 1</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">
+							<i class="icon-bar-chart"></i>
+							Item 3 </a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+			<!-- END SIDEBAR MENU -->
+		</div>
+	</div>
+	<!-- END SIDEBAR -->
+	<!-- BEGIN QUICK SIDEBAR -->
+	<!--Cooming Soon...-->
+	<!-- END QUICK SIDEBAR -->
